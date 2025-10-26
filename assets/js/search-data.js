b658c53ea621c033a357afa9ad3d807a95199cc8
esemsc-am4224 <am4224@ic.ac.uk>
2025-10-26 21:29:16 +0000

    Deploying to gh-pages from @ esemsc-am4224/arastunm@276e09eaa2a55e7e158b04d8b100596ce5592e58 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/arastunm/cv/";
           },
-        },{id: "post-deep-q-learning",
+        },{id: "post-natural-policy-gradient-methods",
+      
+        title: "Natural Policy Gradient Methods",
+      
+      description: "General overview of DRPO and PPO",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/arastunm/blog/2025/npg_methods/";
+        
+      },
+    },{id: "post-deep-q-learning",
       
         title: "Deep Q-Learning",
       
